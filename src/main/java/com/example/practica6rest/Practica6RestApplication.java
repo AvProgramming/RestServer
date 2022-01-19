286fab01ffb5ea6b3ec8828b2cf558a9b15197bd
AvProphet <olegarx2000@gmail.com>
2022-01-19 21:15:55 +0100

    First commit

---
+++ src/main/java/com/example/practica6rest/Practica6RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica6rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica6RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica6RestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practica6rest/Practica6RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica6rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica6RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
