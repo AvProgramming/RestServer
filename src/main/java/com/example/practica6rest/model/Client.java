b76cb07496f949dffc32a402073bbc75dbacbe82
AvProphet <olegarx2000@gmail.com>
2022-01-20 22:06:20 +0100

    Created client entity

---
+++ src/main/java/com/example/practica6rest/model/Client.java
@@ -0,0 +1,61 @@
+package com.example.practica6rest.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private Integer phone_number;
+
+    public Client(String name, String email, Integer phone_number) {
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(Integer phone_number) {
+        this.phone_number = phone_number;
+    }
+}
