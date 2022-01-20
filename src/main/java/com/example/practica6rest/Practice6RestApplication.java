fa890d39996c26d35a1f965c2ec2382bcb2635ae
mrkotor2 <mrkotor2@gmail.com>
2022-01-20 21:28:39 +0100

    Added app props

--- src/main/java/com/example/practica6rest/Practice6RestApplication.java
+++ src/main/java/com/example/practica6rest/Practice6RestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Practica6RestApplication {
+public class Practice6RestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Practica6RestApplication.class, args);
+        SpringApplication.run(Practice6RestApplication.class, args);
     }
 
 }
