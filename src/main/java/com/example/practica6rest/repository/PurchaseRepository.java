517fb0f150a704567446cf7d467f782bbc97ff38
AvProphet <olegarx2000@gmail.com>
2022-02-17 21:21:49 +0100

    Removed deployment properties

--- src/main/java/com/example/practica6rest/repository/PurchaseRepository.java
+++ src/main/java/com/example/practica6rest/repository/PurchaseRepository.java
@@ -5,7 +5,10 @@ import com.example.practica6rest.model.enumeral.PurchaseStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
